f402d3ec1dd32be8cee96e5c3cd78d7871d19fb7
Kamranislam0123 <Kamranislam8@gmail.com>
2023-01-04 23:01:01 +0600

    Heading added

---
+++ .history/src/component/TopBanner/TopBanner_20230104192143.js
@@ -0,0 +1,28 @@
+import React, { Component, Fragment } from 'react';
+import '../../asset/css/custom.css'
+import '../../asset/css/bootstrap.min.css'
+import {Button, Col, Container, Row,} from "react-bootstrap";
+
+export default class TopBanner extends Component {
+
+  render() {
+    return (
+      <Fragment>
+        <Container fluid={true} className="topFixBanner p-0 ">
+            <div className="topBannerOverlay">
+                <Container className={"topContent"}>
+                    <Row>
+                        <Col className='text-center'>
+                            <h1> Software Engineer</h1>
+                            <h5> Mobile & Web Application </h5>
+                            <Button variant="primary">Primary</Button>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
+
+        </Container>
+        </Fragment>
+    )
+  }
+}
