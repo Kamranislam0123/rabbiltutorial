336ffd4a9ffdca1ad95002767564687a724f1f89
Kamranislam0123 <Kamranislam8@gmail.com>
2023-01-04 23:21:49 +0600

    topTitles Added

--- src/component/TopBanner/TopBanner.js
+++ src/component/TopBanner/TopBanner.js
@@ -13,9 +13,9 @@ export default class TopBanner extends Component {
                 <Container className={"topContent"}>
                     <Row>
                         <Col className='text-center'>
-                            <h1> Software and Engineer</h1>
-                            <h5> Mobile & Web Application </h5>
-                            <Button variant="primary">Primary</Button>
+                            <h1 className="topTitle"> Software and Engineer</h1>
+                            <h5 className='subTitle' > Mobile & Web Application </h5>
+                            <Button variant="primary">More Info</Button>
                         </Col>
                     </Row>
                 </Container>
